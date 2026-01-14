79e38ac89979078f4a660306c8db73e0e21c8c03
AAYUSH PRATAP BANSAL <aayushpratapbansal@gmail.com>
2026-01-14 21:47:37 +0530

    Refactor employee array functions
    Refactor functions to use array methods for better readability and maintainability.

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-/** @format */
-
-let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
-];
-
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map(emp => {
+    if (emp.profession === "developer") {
+      console.log(emp);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(emp => {
+    if (emp.profession === "developer") {
+      console.log(emp);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  let result = arr.filter(emp => emp.profession !== "admin");
+  console.log(result);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let newArr = [
+    { id: 5, name: "alex", age: "22", profession: "developer" },
+    { id: 6, name: "bob", age: "21", profession: "intern" },
+    { id: 7, name: "emma", age: "23", profession: "admin" },
+  ];
+
+  let finalArr = arr.concat(newArr);
+  console.log(finalArr);
 }
